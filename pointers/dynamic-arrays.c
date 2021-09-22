d7247e141735d2baf21207539569bb475c597d99
harshita200203 <91184115+harshita200203@users.noreply.github.com>
2021-09-22 14:35:44 +0530

    Update dynamic-arrays.c

--- pointers/dynamic-arrays.c
+++ pointers/dynamic-arrays.c
@@ -1,2 +1,32 @@
 
-int main
+#include <stdio.h>
+
+void tolowercase (char *c)
+{
+    if (*c>='A' && *c<='Z'){
+        *c=*c+('a'-'A');
+        printf("the letter in tolowercase is %c", *c);
+        
+    }
+      else 
+      {
+          printf ("the letter is already in tolowercase");
+          
+      }
+}
+    
+    int main()
+{
+    
+    char a;
+    scanf("%c",&a);
+    tolowercase (&a);
+    
+    printf ("\n %d, a");
+    return(0);
+    
+}
+
+
+
+
